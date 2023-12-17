127f84f44ecb75dc6705c10d13d8c6b89de3be55
Lukas Lehr <lukaslehr@hotmail.com>
2023-12-18 00:21:40 +0100

    day 14, 15

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,46 +1,103 @@
 use std::fs::read_to_string;
 
-use hashbrown::HashSet;
-
 fn main(){
     let input = read_to_string("input/input14.txt").unwrap();
-    let part1 = solve(&input);
+    let part1_grid = roll(parse_grid(&input));
 
-    println!("Part 1: {}", part1);
+    let mut grid = parse_grid(&input);
+    let mut cycle_weights : Vec<usize> = Vec::new();
+
+    for _cycle in 0..=1000{
+        cycle_weights.push(get_load(&grid));
+
+        for _direction in 0..4{
+            grid = roll(grid);
+            grid = rotate_grid(&grid);
+        }
+    }
+
+    let mut cycle_length = 0;
+
+    for i in (0..cycle_weights.len() - 2).rev(){
+        if cycle_weights[i] == cycle_weights[cycle_weights.len()-1] {
+            cycle_length = (cycle_weights.len()-1) - i;
+            break;
+        }
+    }
+
+    let mut result = 0;
+    let remainder = 1000000000 % cycle_length;
+
+    for i in (0..cycle_weights.len() - 1).rev() {
+        if i % cycle_length == remainder {
+            result = cycle_weights[i];
+            break;
+        }
+    }
+
+    println!("Day 14: Part 1: {}, Part 2: {}", get_load(&part1_grid), result);
+
 }
 
-fn solve(input : &str) -> usize{
-    let mut weights : usize = 0;
-    let mut rocks : HashSet<(usize, usize)> = HashSet::new();
-    let input = input.split("\r\n");
-    let length = &input.clone().count();
+fn parse_grid(input_str: &str) -> Vec<Vec<char>> {
+    input_str
+        .lines()
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|line| line.trim())
+        .filter(|line| !line.is_empty())
+        .map(|line| line.chars().collect())
+        .collect()
+}
 
-    println!("Length: {}", length);
+fn rotate_grid(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut result: Vec<Vec<char>> = vec![vec!['.'; grid.len()]; grid[0].len()];
+    for col in 0..grid[0].len() {
+        for row in 0..grid.len() {
+            result[col][grid[0].len() - 1 - row] = grid[row][col];
+        }
+    }
+    result
+}
 
-    for (i, line) in input.into_iter().enumerate(){
-        for (j, c) in line.chars().into_iter().enumerate(){
-            match c {
+fn roll(mut grid: Vec<Vec<char>>) -> Vec<Vec<char>>{
+    for row in 0..grid.len(){
+        for col in 0..grid[0].len(){
+            match grid[row][col] {
                 'O' => {
-                    let mut inserted = false;
-                    for looking_up in (0..=i).rev(){
-                        if rocks.contains(&(looking_up,j)) {
-                            inserted = rocks.insert((looking_up+1, j));
-                            weights += length - (looking_up + 1);
-                            break;
+                    for new_row in (0..=row).rev(){
+                        let mut moved = false;
+                        if (grid[new_row][col]) == '#' || (grid[new_row][col]) == 'O'{
+                            if new_row != row {
+                                grid[row][col] = '.';
+                                grid[new_row+1][col] = 'O';
+                                moved = true;
+                            }
                         }
+                        else if new_row == 0 {
+                            grid[row][col] = '.';
+                            grid[0][col] = 'O';
+                            moved = true;
+                        }
+                        if moved { break; }
                     }
-                    if !inserted { 
-                        rocks.insert((i,j)); 
-                        weights += length - i;
-                    }
-                },
-                '#' => {
-                    rocks.insert((i, j));
                 }
                 _ => {},
             }
         }
-        println!(" {} ", weights);
     }
-    weights
+    grid
+}
+
+fn get_load(grid : &Vec<Vec<char>>) -> usize{
+    let mut weight = 0;
+
+    for i in 0..grid.len(){
+        for j in 0..grid[0].len(){
+            if grid[i][j] == 'O'{
+                weight += grid.len() - i;
+            }
+        }
+    }
+    weight
 }
---
+++ src/bin/15.rs
@@ -0,0 +1,87 @@
+use std::fs::read_to_string;
+
+use hashbrown::HashMap;
+
+fn main(){
+    let input = read_to_string("input/input15.txt").unwrap();
+    println!("Day 15: Part 1: {}, Part 2: {}", part1(&input), part2(&input));
+}
+
+fn part1(input : &str) -> u32{
+    input.split(",").map(| line | {
+        get_hash(line)
+    }).sum()
+}
+
+fn part2(input : &str) -> u32{
+
+    let mut map : HashMap<u32, Vec<(&str, u32)>> = HashMap::new();
+
+    input.split(",").for_each(|line| {
+        if line.contains("=") {
+            let (label, focal_string) = line.split_once("=").unwrap();
+            let label_hash = get_hash(label);
+            let focal = focal_string.parse::<u32>().unwrap();
+            if map.contains_key(&label_hash) {
+                let mut list: Vec<(&str, u32)> = map.get(&label_hash).unwrap().to_vec();
+                let mut inserted = false;
+                for i in 0..list.len(){
+                    if list[i].0 == label {
+                        list[i] = (label, focal);
+                        inserted = true;
+                    }
+                }
+                if !inserted {
+                    list.push((label, focal));
+                }
+                map.insert(label_hash, list);
+
+            } else {
+                map.insert(label_hash, vec![(label, focal)]);
+            }
+        } else {
+            let label = line.split_once("-").unwrap().0;
+            let label_hash = get_hash(label);
+            if map.contains_key(&label_hash) {
+                let mut list = map.get(&label_hash).unwrap().to_vec();
+                let mut removed = false;
+                for i in 0..list.len() {
+                    if list[i].0 == label {
+                        list.remove(i);
+                        removed = true;
+                        break;
+                    }
+                }
+                if removed {
+                    map.insert(label_hash, list);
+                }
+
+            }
+        }
+        
+    });
+
+    let mut value = 0;
+    for key in map.keys(){
+        let mut focusing_power = 0;
+        let list = map.get(key).unwrap();
+        for i in 0..list.len(){
+            focusing_power += (key + 1) * (i as u32+1) * list[i].1
+        }
+        value += focusing_power
+    }
+
+    value
+}
+
+fn get_hash(input :&str) -> u32{
+    let mut value = 0;
+    input.chars().for_each(|c| {
+        value += c as u32;
+        value = value * 17;
+        value = value % 256 
+    });
+
+    value
+}
+
